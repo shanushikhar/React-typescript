3055031c7c619cafe2cf0ff108c42b1b22f1b437
Shanu Shikhar <thisisshanushikhar@gmail.com>
2022-10-17 02:59:57 +0530

    Added button click event

---
+++ src/components/EventProps.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+type Event = {
+  clickedOneButton: () => void;
+  clickedSecondButton: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  clickedThirdButton: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    id: number
+  ) => void;
+  clickedStringButton: (name: string) => void;
+};
+
+export default function EventProps(props: Event) {
+  return (
+    <div>
+      <button onClick={props.clickedOneButton}>Button 1</button>
+      <section>
+        <button onClick={(val) => props.clickedSecondButton(val)}>
+          Button 2 with event
+        </button>
+        <button onClick={() => props.clickedStringButton("Well hello there")}>
+          Button 2 with string
+        </button>
+      </section>
+      <section>
+        <button onClick={(e) => props.clickedThirdButton(e, 2)}>
+          Button 3 with event + number
+        </button>
+      </section>
+    </div>
+  );
+}
